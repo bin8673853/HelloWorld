2d8bddc734b929fc608f91699b878baf882c5043
2276225819 <2276225819@qq.com>
2014-10-08 09:09:27 +0800

    ww
    aa

---
+++ Classes/GameLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  GameLayer.cpp
+//  ddgame
+//
+//  Created by mimix on 14-10-8.
+//
+//
+
+#include "GameLayer.h"
---
+++ Classes/GameLayer.h
@@ -0,0 +1,14 @@
+//
+//  GameLayer.h
+//  ddgame
+//
+//  Created by mimix on 14-10-8.
+//
+//
+
+#ifndef __ddgame__GameLayer__
+#define __ddgame__GameLayer__
+
+#include <iostream>
+
+#endif /* defined(__ddgame__GameLayer__) */
